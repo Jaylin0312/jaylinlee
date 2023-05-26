60798bf41b0597704beaf420c44752427ce73e58
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-05-27 03:20:08 +0800

    feat: added 3d coffee cup

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,17 +1,30 @@
+'use client'
 import Image from 'next/image'
+import Spline from '@splinetool/react-spline'
+import styles from './styles.module.css';
 
 export default function About() {
-    return (
-        <section className="flex flex-wrap-reverse py-4 justify-center items-center md:flex-nowrap md:items-center md:justify-between md:py-12 mx-auto max-w-screen-xl">
-            <div className='flex flex-col gap-4 flex-wrap'>
-                <div className='text-lg lg:text-3xl 2xl:text-6xl text-white'>Hello, my name is</div>
-                <div className='text-2xl md:text-5xl lg:text-6xl 2xl:text-8xl font-bold animate-text bg-gradient-to-r from-[#1CDECA] via-[#C985FF] to-[#1CDECA] bg-clip-text text-transparent'>JAYLIN LEE JIE LING</div>
-                <div className='text-lg lg:text-3xl 2xl:text-6xl text-white'>I am also a Web Developer</div>
-                <div className='text-xs md:max-w-lg lg:text-lg 2xl:text-xl text-white'>Second year Information Systems student at Singapore Management University who is eager to explore the field of software engineering</div>
-            </div>
-            <div className='max-w-full h-auto'>
-                <Image src="/Avatar.png" alt="myImage" width={500} height={100}/>
-            </div>
-        </section>
-    )
+  return (
+    <section className="flex flex-wrap-reverse py-4 justify-center items-center md:flex-nowrap md:items-center md:justify-between md:py-12 mx-auto max-w-screen-xl">
+      <div className="flex flex-col gap-4 flex-wrap">
+        <div className="text-lg lg:text-3xl 2xl:text-6xl text-white">
+          Hello, my name is
+        </div>
+        <div className="text-2xl md:text-6xl lg:text-7xl 2xl:text-8xl font-bold animate-text bg-gradient-to-r from-[#1CDECA] via-[#C985FF] to-[#1CDECA] bg-clip-text text-transparent">
+          JAYLIN LEE JIE LING
+        </div>
+        <div className="text-lg lg:text-3xl 2xl:text-6xl text-white">
+          I am also a Web Developer
+        </div>
+        <div className="text-xs md:max-w-lg lg:text-lg 2xl:text-xl text-white">
+          Second year Information Systems student at Singapore Management
+          University who is eager to explore the field of software engineering
+        </div>
+      </div>
+      <div className="max-w-full h-auto relative">
+        <div className={styles.customspline}><Spline className={styles.spline} scene="https://prod.spline.design/X-4TSO3nnhEBcoxR/scene.splinecode" /></div>
+        <Image className='mr-10' src="/Avatar.png" alt="myImage" width={400} height={100} />
+      </div>
+    </section>
+  )
 }
