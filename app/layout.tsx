7edcb7f511d03a9a9acd4825d117f67e0a9fb726
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-07-10 22:17:43 +0800

    fix: fixed og image metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,10 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Jaylin's Portfolio",
-  description: 'Personal portfolio and journey as a computing student',
+  title: 'Jaylin Lee',
+  description: 'Personal Portfolio Site',
+  webkitURL: 'https://jaylinlee.dev',
+  imageURL: '/opengraph-image.png',
 }
 
 export default function RootLayout({
@@ -20,12 +22,15 @@ export default function RootLayout({
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:url" content="https://jaylinlee.dev" />
+        <meta property="og:url" content={metadata.webkitURL} />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/opengraph-image.png" />
-        <meta property="og:image:type" content="/opengraph-image.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        <meta
+          property="og:image"
+          content={`${metadata.webkitURL}${metadata.imageURL}`}
+        />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
       </Head>
       <html className="scroll-smooth" lang="en">
         <body className={inter.className}>{children}</body>
