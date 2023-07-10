6a69643b71e91ffd09fb3445ab99e7d9e7431058
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-07-10 21:24:39 +0800

    feat: added cover picture

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Jaylin's Portfolio",
-  description: 'This is Jaylin\'s portfolio',
+  description: 'Personal portfolio and journey as a computing student',
 }
 
 export default function RootLayout({
@@ -20,6 +20,12 @@ export default function RootLayout({
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="https://jaylinlee.dev" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/opengraph-image.png" />
+        <meta property="og:image:type" content="/opengraph-image.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
       <html className="scroll-smooth" lang="en">
         <body className={inter.className}>{children}</body>
